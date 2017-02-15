ea5ccf042f153573335db87c2563fa4dea1666fc
Mark Boddington <tuxinvader@users.noreply.github.com>
2017-02-15 19:06:51 +0000

    set pins to some values, and attempt overdrive

--- test1.py
+++ test1.py
@@ -8,15 +8,23 @@
 from w1ftdi import W1ftdi
 from ds18b20 import Ds18b20
 
-debug = 5  # debug level 0 to 5
+debug = 2  # debug level 0 to 5
 pin   = 8  # pin c0
+overdrive = True
 
+# Open and initialise the FT232h for 1-Wire
 print "TEST 1: Perform Rom Search..."
-w1 = W1ftdi(pin, debug)
+w1 = W1ftdi(pin, debug, overdrive)
 w1.open()
 w1.sync()
 w1.setup_clock()
 
+# set some arbitrary GPIO states, and write them.
+w1.set_pin(3, True, False)
+w1.set_pin(7, True, False)
+w1.write_gpio_state()
+
+# Search for devices on the 1-Wire bus
 if w1.reset():
     roms = w1.search_roms()
     print "Found roms: {}".format(roms)
